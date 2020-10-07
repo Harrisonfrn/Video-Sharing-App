cf895e832491975ca35d042ea202864f4a3f4983
HarrisonPro <frnharrison@gmail.com>
2020-10-07 09:08:02 +0200

    composer update

--- src/Utils/CategoryTreeFrontPage.php
+++ src/Utils/CategoryTreeFrontPage.php
@@ -2,6 +2,7 @@
 
 namespace App\Utils;
 
+use App\Twig\AppExtension;
 use App\Utils\AbstractClasses\CategoryTreeAbstract;
 
 class CategoryTreeFrontPage extends CategoryTreeAbstract{
@@ -14,16 +15,34 @@ class CategoryTreeFrontPage extends CategoryTreeAbstract{
     public $html_6 = '</li>';
     public $html_7 = '</ul>';
 
+    public function getCategoryListAndParent(int $id): string
+    {
+        $this->slugger = new AppExtension; //Extension slugify url's for categories
+
+        $parentData = $this->getMainParent($id);
+        $this->mainParentName = $parentData['name'];
+        $this->mainParentId = $parentData['id'];
+        $key = array_search($id, array_column($this->categoriesArrayFromDb, 
+        'id'));
+        $this->currentCategoryName = $this->categoriesArrayFromDb[$key]['name'];
+
+        $categories_array = $this->buildTree($parentData['id']);
+
+        return $this->getCategoryList($categories_array);
+    }
+
     public function getCategoryList(array $categories_array)
     {
-
-
         $this->categorylist .= $this->html_1;
 
         foreach ($categories_array as $value ) {
-            $catName = $value['name'];
-            $url = $this->urlGenerator->generate('videoList', ['categoryname' =>$catName, 'id'=>$value['id']]);
-            $this->categorylist .= $this->html_2 . $this->html_3 . $url .  $this->html_4 .  $catName . $this->html_5;
+            
+            $catName = $this->slugger->slugify($value['name']);
+
+            $url = $this->urlGenerator->generate('videoList', 
+            ['categoryname' =>$catName, 'id'=>$value['id']]);
+            $this->categorylist .= $this->html_2 . $this->html_3 . 
+            $url .  $this->html_4 .  $catName . $this->html_5;
 
             if (!empty($value['children'])) {
                 $this->getCategoryList($value['children']);
@@ -40,6 +59,18 @@ class CategoryTreeFrontPage extends CategoryTreeAbstract{
     public function getMainParent(int $id): array
     {
         $key = array_search($id, array_column($this->categoriesArrayFromDb, 'id'));
+
+        if ($this->categoriesArrayFromDb[$key]['parent_id'] != null) {
+            return $this->getMainParent($this->categoriesArrayFromDb[$key]
+                ['parent_id']);
+        }
+        else
+        {
+            return [
+                'id'=>$this->categoriesArrayFromDb[$key]['id'],
+                'name'=>$this->categoriesArrayFromDb[$key]['name']
+        ];
+        }
     }
 
 
